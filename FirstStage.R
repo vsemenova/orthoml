916d0f841f201616578d8ac27da83736d25a25c9
Vira Semenova <vira_semenova@fas.harvard.edu>
2022-11-25 18:53:34 -0500

    updated

--- FirstStage.R
+++ FirstStage.R
@@ -7,17 +7,8 @@ first_stage<-function(treat,outcome,mydata,
   fe_pdata<-model.matrix(first_stage_price_formula,data=my_data )
   controls<-as.matrix(fe_pdata)
   
-  n_items<-length(grep("Item",colnames(fe_pdata),value=TRUE))
-  ### unpenalize treatment interactions with logprice_lag and logprice_lag_2
-  penalty.factor.treat=c(0,rep(1/sqrt(n_items),n_items),rep(1,length(colnames(fe_pdata))-n_items-9),rep(0,8))
-  lambda.treat=2*(log(length(inds_train)))^(3/2)/sqrt(length(inds_train))
-  
-  
-  
   t.treat<-remove_wrapper(controls = controls,
                           target =treat,
-                          lambda=lambda.treat,
-                          penalty.factor=penalty.factor.treat,
                           inds_train=inds_train,...)
   res.treat<-t.treat$res
   treat.fit<-t.treat$fit.c
@@ -29,15 +20,10 @@ first_stage<-function(treat,outcome,mydata,
   
   fe_pdata<-model.matrix(first_stage_sales_formula,data=my_data )
   controls<-as.matrix(fe_pdata)
-  n_items<-length(grep("Item",colnames(fe_pdata),value=TRUE))
-  penalty.factor.outcome=c(0,rep(1/sqrt(n_items),n_items),rep(1,length(colnames(fe_pdata))-n_items-1))
-  lambda.outcome=my_cons*(log(length(inds_train)))^(3/2)/sqrt(length(inds_train))
   
   
   t.out<-remove_wrapper(controls = controls,
                         target = outcome,
-                        lambda=lambda.outcome,
-                        penalty.factor=penalty.factor.outcome,
                         inds_train=inds_train,...)
   res.outcome<-t.out$res
   outcome.fit<-t.out$fit.c
